283af835281293029f4aef4e93af890a082e307a
babybreath <jai2012@163.com>
2016-03-03 23:01:43 +0800

    change URL

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,40 +1,45 @@
 #!/bin/sh
 
-mkdir nginx_soft
+# 变量申明
 filepath=$(cd "$(dirname "$0")"; pwd)
-nginx_prefix=$filepath/nginx_soft
+nginx_prefix=$filepath/nginx_dir
 
+pcreVersion=pcre-8.38
+pcreVersionNumber=8.38
+zlibVersion=zlib-1.2.8
+opensslVersion=openssl-1.0.2g
+nginxVersion=nginx-1.8.1
+
+pcreUrl=http://jaist.dl.sourceforge.net/project/pcre/pcre/$pcreVersionNumber/$pcreVersion.tar.gz
+zlibUrl=http://zlib.net/$zlibVersion.tar.gz
+opensslUrl=http://www.openssl.org/source/$opensslVersion.tar.gz
+nginxUrl=http://nginx.org/download/$nginxVersion.tar.gz
+
+mkdir $nginx_prefix
 cd $nginx_prefix
 
-pcreUrl=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz
-zlibUrl=http://zlib.net/zlib-1.2.8.tar.gz
-opensslUrl=http://www.openssl.org/source/openssl-1.0.1.tar.gz
-nginxUrl=http://nginx.org/download/nginx-1.8.1.tar.gz
 
+echo "获取pcre源码"
 wget $pcreUrl
-
+echo "获取zlib源码"
 wget $zlibUrl
-
+echo "获取openssl源码"
 wget $opensslUrl
-
+echo "获取nginx源码"
 wget $nginxUrl
+echo "解压pcre源码"
+tar -zxf $pcreVersion.tar.gz
+echo "解压zlib源码"
+tar -zxf $zlibVersion.tar.gz
+echo "解压openssl源码"
+tar -zxf $opensslVersion.tar.gz
+echo "解压nginx源码"
+tar -zxf $nginxVersion.tar.gz
 
-tar -zxvf pcre-8.38.tar.gz
 
-tar -zxvf zlib-1.2.8.tar.gz
-
-tar -zxvf openssl-1.0.1.tar.gz
-
-tar -zxvf nginx-1.8.1.tar.gz
-
-cd nginx-1.8.1
-
-./configure 
---prefix=$nginx_prefix/nginx 
---with-http_ssl_module 
---with-openssl=$nginx_prefix/openssl-1.0.1 
---with-pcre=$nginx_prefix/pcre-8.38 
---with-zlib=$nginx_prefix/zlib-1.2.8
+cd $nginxVersion
+echo "开始安装nginx"
+./configure --prefix=$nginx_prefix/nginx --with-http_ssl_module --with-openssl=$nginx_prefix/$opensslVersion --with-pcre=$nginx_prefix/$pcreVersion --with-zlib=$nginx_prefix/$zlibVersion
 
 make & make install
 
