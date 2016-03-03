e6f43af3685b89f4478ad2829f6ce3e8c2ef6f4a
babybreath <jai2012@163.com>
2016-03-04 00:26:31 +0800

    使用{}包裹变量

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ linux脚本
 	source node_install.sh [parameter]
 
 parameter 可选的安装路径 默认 /usr/node/latest
+
+## nginx_install.sh
+
+自动化安装nginx
+
+在当前路径下新建目录nginx_dir
+nginx安装目录为nginx_dir/nginx
--- nginx_install.sh
+++ nginx_install.sh
@@ -10,10 +10,10 @@ zlibVersion=zlib-1.2.8
 opensslVersion=openssl-1.0.2g
 nginxVersion=nginx-1.8.1
 
-pcreUrl=http://jaist.dl.sourceforge.net/project/pcre/pcre/$pcreVersionNumber/$pcreVersion.tar.gz
-zlibUrl=http://zlib.net/$zlibVersion.tar.gz
-opensslUrl=http://www.openssl.org/source/$opensslVersion.tar.gz
-nginxUrl=http://nginx.org/download/$nginxVersion.tar.gz
+pcreUrl=http://jaist.dl.sourceforge.net/project/pcre/pcre/${pcreVersionNumber}/${pcreVersion}.tar.gz
+zlibUrl=http://zlib.net/${zlibVersion}.tar.gz
+opensslUrl=http://www.openssl.org/source/${opensslVersion}.tar.gz
+nginxUrl=http://nginx.org/download/${nginxVersion}.tar.gz
 
 mkdir $nginx_prefix
 cd $nginx_prefix
@@ -28,18 +28,18 @@ wget $opensslUrl
 echo "获取nginx源码"
 wget $nginxUrl
 echo "解压pcre源码"
-tar -zxf $pcreVersion.tar.gz
+tar -zxf ${pcreVersion}.tar.gz
 echo "解压zlib源码"
-tar -zxf $zlibVersion.tar.gz
+tar -zxf ${zlibVersion}.tar.gz
 echo "解压openssl源码"
-tar -zxf $opensslVersion.tar.gz
+tar -zxf ${opensslVersion}.tar.gz
 echo "解压nginx源码"
-tar -zxf $nginxVersion.tar.gz
+tar -zxf ${nginxVersion}.tar.gz
 
 
 cd $nginxVersion
 echo "开始安装nginx"
-./configure --prefix=$nginx_prefix/nginx --with-http_ssl_module --with-openssl=$nginx_prefix/$opensslVersion --with-pcre=$nginx_prefix/$pcreVersion --with-zlib=$nginx_prefix/$zlibVersion
+./configure --prefix=${nginx_prefix}/nginx --with-http_ssl_module --with-openssl=${nginx_prefix}/${opensslVersion} --with-pcre=${nginx_prefix}/${pcreVersion} --with-zlib=${nginx_prefix}/${zlibVersion}
 
 make & make install
 
